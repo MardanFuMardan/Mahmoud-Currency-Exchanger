9801774ad8362c949328520ac111018180bd0867
MardanFuMardan <mahmudhegazy007@gmail.com>
2024-10-19 03:49:14 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,18 @@ export default defineConfig({
     loader: 'jsx',  // تمكين دعم JSX
     include: /src\/.*\.jsx?$/,  // تعريف الملفات التي تحتاج JSX
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // تسمية ملفات JavaScript للتأكد من أن Netlify يتعامل معها بشكل صحيح
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  },
+  server: {
+    // يساعد أحيانًا في التعامل مع المشاكل أثناء التطوير
+    strictPort: true,
+  }
 })
